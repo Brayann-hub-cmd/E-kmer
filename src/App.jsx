62498698f54ee5b157cdd131034e666045669ffa
WillR3n <fogouwilliam4@gmail.com>
2026-03-04 03:59:11 +0100

    Modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-
+import SignUp from "./Auth/Auth"
 function App() {
-  
+  return <SignUp/>;
 }
 
 export default App
---
+++ src/Auth/Auth.jsx
@@ -0,0 +1,10 @@
+function SignUp() {
+    return(
+        <div className="signup" data-aos="fade-up">
+            <h1 className="bg-red-500">Will</h1>
+            <button className="btn btn-primary" data-aos="fade-up">S'inscrire</button>
+        </div>
+    )
+}
+
+export default SignUp;
