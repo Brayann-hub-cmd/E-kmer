1f9a5d90df37eefb1cdb7465ddb9d8fbd70b4e10
Brayann-hub-cmd <feunguetsamobrayann@gmail.com>
2026-03-10 23:07:24 +0100

    Modification de la page d'inscription

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import Login from "./pages/auth/Login";
-import SignUp from "./SignUp/SignUp";
+import SignUp from "./pages/SignUp/SignUp"
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 function App() {
   const router = createBrowserRouter([
